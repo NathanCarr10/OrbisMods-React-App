56a658bcad59a9ddd51923f6fcc206f7ab4756f4
NathanCarr10 <nathancarr18@gmail.com>
2025-03-26 16:27:05 +0000

    added firebase config information to firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDqvYxcN6bD4V5q3E0hxjN6WEI4vCU1rzc",
+  authDomain: "orbis-mods.firebaseapp.com",
+  projectId: "orbis-mods",
+  storageBucket: "orbis-mods.firebasestorage.app",
+  messagingSenderId: "804452887090",
+  appId: "1:804452887090:web:b4a0b862bfe0eb8bdaaa2b",
+  measurementId: "G-HVQ81BPXZ6"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 
-export default firebaseConfig;
+  
