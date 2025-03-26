c70b32d0ba6ce5c99463159e9f231e7dfc9eed27
NathanCarr10 <nathancarr18@gmail.com>
2025-03-26 15:46:36 +0000

    created firebase.js for login functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // Importing necessary components from 'react-router-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useAuth } from "./context/AuthContext";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 // Importing Bootstrap CSS for styling the application
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Importing various custom components used in the app
 import Homepage from './Components/Homepage';
-import LoginPage from './Components/LoginPage';
+import Login from './Components/LoginPage';
 import Products from './Components/Products';
 import ShoppingCart from './Components/ShoppingCart';
 import UserAccount from './Components/UserAccount';
@@ -20,7 +23,7 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/LoginPage" element={<Login />} />
         <Route path="/Products" element={<Products />} />
         <Route path="/ShoppingCart" element={<ShoppingCart/>} />
         <Route path="/UserAccount" element={<UserAccount />} />
