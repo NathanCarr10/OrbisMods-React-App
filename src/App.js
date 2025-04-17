1172a63304e521dac8d18306c11793f98b339efa
JoelFx <G00408551@atu.ie>
2025-04-17 12:53:46 +0100

    add AboutPage import and route in App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import UserAccount from './Components/UserAccount';
 import NavigationBar from './Components/NavigationBar';
 import OrderHistory from './Components/OrderHistory';
 import CheckoutPage from './Components/CheckoutPage';
+import AboutPage from "./Components/AboutPage";
 
 // Stripe integration
 import { Elements } from '@stripe/react-stripe-js';
@@ -115,6 +116,7 @@ function App() {
           <Route path="/LoginPage" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/Products" element={<Products />} />
+          <Route path="/About" element={<AboutPage />} />
 
           {/* Protected route for user account */}
           <Route
